35f89d3ce7fe7d2dd045c0d5536a30360441f859
upkl <2737384+upkl@users.noreply.github.com>
2023-09-08 20:51:37 +0200

    some renames

--- app/javascript/controllers/exercise_controller.js
+++ app/javascript/controllers/exercise_controller.js
@@ -1,11 +1,11 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["toggled"];
+  static targets = ["toToggle"];
   static classes = ["change"];
 
   toggle () {
-    for (let target of this.toggledTargets) {
+    for (let target of this.toToggleTargets) {
       target.classList.toggle(this.changeClass);
     };
   };
